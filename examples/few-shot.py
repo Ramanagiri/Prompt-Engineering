8a3134ec80fc694529690698bfeb64fed1d2f4c3
Ramanagiri <112062298+Ramanagiri@users.noreply.github.com>
2026-04-18 23:03:03 +0530

    Create few-shot.py

---
+++ examples/few-shot.py
@@ -0,0 +1,20 @@
+response = client.chat.completions.create(
+    model="gpt-5.3",
+    messages=[
+        {
+            "role": "user",
+            "content": """Classify sentiment:
+
+Text: I hate this movie
+Sentiment: Negative
+
+Text: This is amazing
+Sentiment: Positive
+
+Text: It is okay
+Sentiment:"""
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
